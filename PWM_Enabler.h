e2af70cd995bc816bb1d0c51cc675d25523f3b34
Roman Nelson <romannels@gmail.com>
2014-09-10 09:01:21 -0500

    Added comments

--- PWM_Enabler.h
+++ PWM_Enabler.h
@@ -12,12 +12,13 @@
 #define PWM_0A 0x154 //EHRPWM 0A Offset
 #define PWM_CLOCK_ENABLE 0x2 // Enable PWM 
 #define PWM_DIR "/sys/devices/ocp.2/pwm_test_P8_13.14" // Device directory
-#define MAX_BUF 64
+#define MAX_BUF 64 // for serial buffer
 #define MAX_STRING_LENGTH 15 // For conversion between integer and string
 #define PERIOD_BASIS 1000000 // Corresponds to 1 ms period
 #define MAX_PERIOD 100 // Arbitrarily selected TODO NEED TO RESEARCH MAX PERIOD
 #define MAX_COMMAND_LENGTH 10 // for serial input
 
-void enable_PWM(void);
+/* PROTOTYPES */
+void enable_PWM(void); 
 
 #endif
